621e1d5d3b9bad17d56e2766f33b049491ca46c1
pasathbandara <101523910+pasathbandara@users.noreply.github.com>
2023-01-30 23:50:33 +0530

    First Java Code

---
+++ Test.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] b = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] c = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+        System.out.println(Arrays.toString(c));
+    }
+}
