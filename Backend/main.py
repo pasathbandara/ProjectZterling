3ba352f0ff48ee6245e8cb0668401dcfd7223d67
Manithnp <manithnp@gmail.com>
2023-02-13 23:17:49 +0530

    manith first commit

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,13 @@
-print("hello world")
+import joblib
+import string 
+from nltk.corpus import stopwords
+
+def process_text(s):
+
+    #check puctuation
+    nopunc =[char for char in s if char not in string.punctuation]
+
+    # join the characters  without punctuation
+    nopunc =''.join(nopunc)
+
+    
