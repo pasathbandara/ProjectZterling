3e8b24d1892ebc6631f78e0fb1cc1c3081b3e25b
blackPhantom81N <nikeshaf19@gmail.com>
2023-02-13 23:50:19 +0530

    Nikesha's Commit in main

--- Backend/main.py
+++ Backend/main.py
@@ -2,12 +2,26 @@ import joblib
 import string 
 from nltk.corpus import stopwords
 
+#Collecting User Text
+user_input = input(str("Please enter the news content you want to verify: "))
+print("You entered:  " + str(user_input))
+
+
 def process_text(s):
 
-    #check puctuation
+    #Check Punctuation
     nopunc =[char for char in s if char not in string.punctuation]
 
-    # join the characters  without punctuation
+    # Join the characters  without punctuation
     nopunc =''.join(nopunc)
 
+    #Converting Strings to Lowercase and Removing stopwords
+    clean_string = [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
+    return clean_string
+
+
+detector_pipeline = joblib.load("detector_pipeline.joblib")
+result = detector_pipeline.predict([['''''']])
+
+print(result)
     
