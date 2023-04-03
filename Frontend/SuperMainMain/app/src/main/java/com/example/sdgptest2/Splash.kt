d9f3b6648358a0079f7e159fc50ffdea83e4ff1a
yasindu20211263 <yasindu.20211263@iit.ac.lk>
2023-04-03 22:20:28 +0530

    Splash Functionality Added

--- Frontend/SuperMainMain/app/src/main/java/com/example/sdgptest2/Splash.kt
+++ Frontend/SuperMainMain/app/src/main/java/com/example/sdgptest2/Splash.kt
@@ -1,11 +1,18 @@
 package com.example.sdgptest2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val actionBtn = findViewById<Button>(R.id.readingbtn)
+        actionBtn.setOnClickListener(){
+            val Intent = Intent(this, MainPage::class.java)
+            startActivity(Intent)
+            finish()}
     }
 }
