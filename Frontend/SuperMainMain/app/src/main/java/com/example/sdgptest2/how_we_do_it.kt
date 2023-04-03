45ca148262a661f0c0c9dc8686d3bdb420acd3a8
pasathbandara <101523910+pasathbandara@users.noreply.github.com>
2023-04-03 21:26:50 +0530

    error cant fix created new

---
+++ Frontend/SuperMainMain/app/src/main/java/com/example/sdgptest2/how_we_do_it.kt
@@ -0,0 +1,11 @@
+package com.example.sdgptest2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class how_we_do_it : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_how_we_do_it)
+    }
+}
