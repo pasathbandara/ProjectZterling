705bb1700c49ced5599c8e4a342fd5923cce5c87
yasindu20211263 <yasindu.20211263@iit.ac.lk>
2023-04-08 13:57:14 +0530

    Excpetion handling application offline mode added

--- Frontend/SuperMainMain/app/src/main/java/com/example/sdgptest2/how_we_do_it.kt
+++ Frontend/SuperMainMain/app/src/main/java/com/example/sdgptest2/how_we_do_it.kt
@@ -2,6 +2,7 @@
 package com.example.sdgptest2
 
 // Import statements
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -20,6 +21,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.POST
+import java.net.NoRouteToHostException
 
 // Class declaration for "how_we_do_it" activity
 class how_we_do_it : AppCompatActivity() {
@@ -53,34 +55,51 @@ class how_we_do_it : AppCompatActivity() {
         // Handle button click
         val actionBtn = findViewById<Button>(R.id.urlcheck)
         actionBtn.setOnClickListener() {
-            val url = editTextTextMultiLine!!.text.toString()
-            val news = editTextTextMultiLine2!!.text.toString()
+            // If the Device is connected to the internet
+            try {
+                println(
+                    "Device is connected to the Internet. " +
+                            "\nApplication Functionalities will be done using Flask API"
+                )
+                val url = editTextTextMultiLine!!.text.toString()
+                val news = editTextTextMultiLine2!!.text.toString()
 
-            // Prepare data for API request
-            val data = mapOf("url" to url, "news" to news)
+                // Prepare data for API request
+                val data = mapOf("url" to url, "news" to news)
 
-            // Send API request
-            api.validateNews(data).enqueue(object : Callback<Map<String, Any>> {
-                override fun onResponse(
-                    call: Call<Map<String, Any>>,
-                    response: Response<Map<String, Any>>
-                ) {
-                    if (response.isSuccessful) {
-                        // Handle successful response
-                        println("Response Generated")
-                        val result = response.body()!!["result"] as String
-                        val reliabilityRating = response.body()!!["reliability_rating"] as Double
-                        true_false!!.text = "$result: $reliabilityRating/5)"
-                        println(result + " " + reliabilityRating)
-                    } else {
-                        println("Response Unsuccessful")
+                // Send API request
+                api.validateNews(data).enqueue(object : Callback<Map<String, Any>> {
+                    override fun onResponse(
+                        call: Call<Map<String, Any>>,
+                        response: Response<Map<String, Any>>
+                    ) {
+                        if (response.isSuccessful) {
+                            // Handle successful response
+                            println("Response Generated")
+                            val result = response.body()!!["result"] as String
+                            val reliabilityRating =
+                                response.body()!!["reliability_rating"] as Double
+                            true_false!!.text = "$result: $reliabilityRating/5)"
+                            println(result + " " + reliabilityRating)
+                        } else {
+                            println("Response Unsuccessful")
+                        }
                     }
-                }
 
-                override fun onFailure(call: Call<Map<String, Any>>, t: Throwable) {
-                    println("Connection Failed \n${t.message}")
-                }
-            })
+                    override fun onFailure(call: Call<Map<String, Any>>, t: Throwable) {
+                        println("Connection Failed \n${t.message}")
+                    }
+                })
+            }
+            // If the Device is not connected to the internet
+            catch (e: NoRouteToHostException) {
+                startChaqoupyFramework(this)
+            }
+            // If there is any other exception
+            catch (e: java.lang.Exception) {
+                print(e.stackTrace)
+                startChaqoupyFramework(this)
+            }
         }
 
         // Handle clicks on other buttons
@@ -122,28 +141,37 @@ class how_we_do_it : AppCompatActivity() {
 
 /*
 ----------------------------- THE BELOW CODE IS TO BE USED WHEN THE USER IS NOT CONNECTED TO INTERNET---------------------------------------------------
+*/
+fun startChaqoupyFramework(context: Context) {
+    println(
+        "Device isn't connected to the Internet. " +
+                "\nApplication Functionalities will be done using chaqoupy framework" +
+                "\nYou might experience crashes and bugs"
+    )
+    if (!Python.isStarted()) Python.start(AndroidPlatform(context))
 
-        if (!Python.isStarted()) Python.start(AndroidPlatform(this))
+    val py = Python.getInstance()
 
-        val py = Python.getInstance()
+    val actionBtn = findViewById<Button>(R.id.urlcheck)
+    actionBtn.setOnClickListener() {
+        actionBtn.setOnClickListener {
+            /*  To Test A Function - Offline Mode
+            simple function for testing
+            val pyobj = py.getModule("hello_user")
+            val obj = pyobj.callAttr("helloworld").toString()
+            true_false!!.text = obj
+            println(obj)
+             */
 
-        val actionBtn = findViewById<Button>(R.id.urlcheck)
-        actionBtn.setOnClickListener(){
-            actionBtn.setOnClickListener {
-                /*  To Test A Function - Offline Mode
-                simple function for testing
-                val pyobj = py.getModule("hello_user")
-                val obj = pyobj.callAttr("helloworld").toString()
-                true_false!!.text = obj
-                println(obj)
-                 */
-
-                val pyobj = py.getModule("main")
-                val obj = pyobj.callAttr("validate_news", editTextTextMultiLine!!.text.toString(), editTextTextMultiLine2!!.text.toString()) as List<PyObject>
-                true_false!!.text = "obj.toString()"
-                println("\n"+obj)
-            }
+            val pyobj = py.getModule("main")
+            val obj = pyobj.callAttr(
+                "validate_news",
+                editTextTextMultiLine!!.text.toString(),
+                editTextTextMultiLine2!!.text.toString()
+            ) as List<PyObject>
+            true_false!!.text = "obj.toString()"
+            println("\n" + obj)
         }
-
- */
+    }
+}
 }
